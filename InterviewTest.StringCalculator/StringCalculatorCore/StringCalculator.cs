b3f800195c68ed1b8e3a00bc397ca1dd6b629a1b
TINYPC\Mark <mlangf1378@aol.com>
2017-03-17 16:26:56 +0000

    initial commit passes 5 tests

---
+++ InterviewTest.StringCalculator/StringCalculatorCore/StringCalculator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculatorCore
+{
+    public static class StringCalculator
+    {
+        const char _Delimeter = ',';
+
+        public static int Add(string inputString, char delimeter = ',')
+        {
+            int val = 0;
+
+            //inputString = inputString.Replace(_Delimeter, '+');
+//            inputString = inputString.Replace(delimeter, '+');
+            string[] numbers = inputString.Split(delimeter);
+
+            string newString = "";
+            for(int index = 0; index < numbers.Length; index++)
+            {
+                if (!numbers[index].Any(char.IsDigit))
+                {
+                    break;
+                }
+                if (Convert.ToInt32(numbers[index]) < 100)
+                {
+                    
+                    newString += numbers[index];
+
+                    if (index < numbers.Length - 1)
+                        newString += "+";
+                }
+
+            }
+
+            try
+            {
+                //string value = new DataTable().Compute(inputString, null).ToString();
+                string value = new DataTable().Compute(newString, null).ToString();
+                if (value.Any(char.IsDigit))
+                {
+                    val = Convert.ToInt32(value);
+                }
+                return val;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }            
+        
+    }
+}
---
+++ InterviewTest.StringCalculator/StringCalculatorKata.Tests/StringCalculatorTests.cs
@@ -0,0 +1,76 @@
+﻿using NUnit.Framework;
+using StringCalculatorCore;
+
+namespace StringCalculatorKata.Tests
+{
+    [TestFixture]
+    public class StringCalculatorTests
+    {
+        [Test]
+        public void Calculate_EmptyString_ReturnsZero()
+        {
+            int expectedResult = 0;
+            int val = StringCalculator.Add("");
+            Assert.AreEqual(expectedResult, val);
+        }
+
+        [Test]
+        //[Ignore("complete previous test first")]
+        public void Calculate_SingleNumber_ReturnsSameNumber()
+        {
+            int expectedResult = 42;
+            int val = StringCalculator.Add("42");
+            Assert.AreEqual(expectedResult, val);
+        }
+
+        [Test]
+        //[Ignore("complete previous test first")]
+        public void Calculate_TwoNumbers_ReturnsSum()
+        {
+            int expectedResult = 6;
+            int val = StringCalculator.Add("3,3");
+            Assert.AreEqual(expectedResult, val);
+        }
+        
+        [Test]
+        //[Ignore("complete previous test first")]
+        public void Calculate_FiveNumbers_ReturnsSum()
+        {
+            int expectedResult = 42;
+            int val = StringCalculator.Add("2,4,9,5,22");
+            Assert.AreEqual(expectedResult, val);
+        }
+   
+        // Additional Functionality
+
+        [Test]
+        //[Ignore("complete previous test first")]
+        public void Calculate_CanUseAlternativeDelimiters_ReturnsSum()
+        {
+            int expectedResult = 42;
+            int val = StringCalculator.Add("2;4;9;5;22", ';');
+            Assert.AreEqual(expectedResult, val);
+        }
+
+        [Test]
+        [Ignore("complete previous test first")]
+        public void Calculate_NumbersLargerThan100_IgnoresOver100sInSum()
+        {
+        
+        }
+
+        [Test]
+        [Ignore("complete previous test first")]
+        public void Calculate_NonNumericValue_ReturnsZero()
+        {
+        
+        }
+
+        [Test]
+        [Ignore("complete previous test first")]
+        public void Calculate_FirstSevenInFibonacciSequence_ThrowsCustomFibonacciAlertException()
+        {
+        
+        }
+    }
+}
